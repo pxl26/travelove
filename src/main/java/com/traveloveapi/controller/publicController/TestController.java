bc4969b7434904b3e1b924aac7df40c21de07948
pxl26 <phanxuanloc2612@gmail.com>
2024-03-14 21:20:08 +0700

    Load testing API

--- src/main/java/com/traveloveapi/controller/publicController/TestController.java
+++ src/main/java/com/traveloveapi/controller/publicController/TestController.java
@@ -1,16 +1,26 @@
 package com.traveloveapi.controller.publicController;
 
+import com.traveloveapi.DTO.activity.ViewedTourDTO;
 import com.traveloveapi.repository.bill.BillRepository;
 import com.traveloveapi.service.BillService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/public/test")
 public class TestController {
-    final private BillRepository billRepository;
-    final private BillService billService;
 
+    @Tag(name = "Load testing")
+    @GetMapping("/empty-get")
+    public String emptyGet() {
+        return "Ok";
+    }
+
+    @Tag(name = "Load testing")
+    @PostMapping("/empty-post")
+    public String emptyPost(@RequestBody ViewedTourDTO data) {
+        return "Ok";
+    }
 }
